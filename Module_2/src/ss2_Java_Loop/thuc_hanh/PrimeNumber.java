f50b17185c2201b74e43dd6e87922d7e107f0871
NgocLinhBuii <ngoclinhh29@gmail.com>
2024-06-10 21:20:50 +0700

    nop bai

---
+++ Module_2/src/ss2_Java_Loop/thuc_hanh/PrimeNumber.java
@@ -0,0 +1,25 @@
+package ss2_Java_Loop.thuc_hanh;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
+        if (number < 2) {
+            System.out.println(number + " is not a prime");
+        }
+        boolean isPrime = true;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                isPrime = false;
+            }
+        }
+        if (isPrime) {
+            System.out.println(number + " is a prime number");
+        } else {
+            System.out.println(number + " is not a prime number");
+        }
+    }
+}
