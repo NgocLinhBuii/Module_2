f9f22ce0c8698124b4dfdbe5590534ee2b331b5b
NgocLinhBuii <ngoclinhh29@gmail.com>
2024-06-10 21:45:28 +0700

    nop bai

---
+++ Module_2/src/ss2_Java_Loop/bai_tap/VeHinh.java
@@ -0,0 +1,36 @@
+package ss2_Java_Loop.bai_tap;
+
+public class VeHinh {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 7; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i <= 5; i++) {
+            for (int j = i; j < 5; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
