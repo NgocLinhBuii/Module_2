9232ab73a4b3c61226d91df5ea9fe185a4e693ac
NgocLinhBuii <ngoclinhh29@gmail.com>
2024-06-10 20:34:41 +0700

    test

--- Module_2/src/ss1_introduction_to_java/thuc_hanh/demo.java
+++ Module_2/src/ss1_introduction_to_java/thuc_hanh/demo.java
@@ -4,11 +4,16 @@ import java.util.Scanner;
 
 public class demo {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Nhập vào 1 số: ");
-        int num;
-        do{
-            num = sc.nextInt();
-        } while(num > 50 || num < 0);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Nhập vào 1 số: ");
+//        int num;
+//        do{
+//            num = sc.nextInt();
+//        } while(num > 50 || num < 0);
+
+        String[] arrString = {"Ngân", "Thương", "Nam"};
+        for (String x : arrString){
+            System.out.println(x);
+        }
     }
 }
