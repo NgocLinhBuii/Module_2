3bf7efc9cfdc83c222606d02f729f1fda149edda
NgocLinhBuii <ngoclinhh29@gmail.com>
2024-06-12 21:59:59 +0700

    nop bai

---
+++ Module_2/src/ss3_array_method/thuc_hanh/Min.java
@@ -0,0 +1,25 @@
+package ss3_array_method.thuc_hanh;
+
+import java.util.Scanner;
+
+public class Min {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int min = min(arr);
+        System.out.println(min);
+}
+    public static int min(int []arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
