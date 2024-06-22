7d9947ec0c37b9eb245e33f9c23e58b2ef927e33
NgocLinhBuii <ngoclinhh29@gmail.com>
2024-06-22 20:10:51 +0700

    nop bai

---
+++ Module_2/src/ss5_accessModifier/bai_tap/TestCircle.java
@@ -0,0 +1,10 @@
+package ss5_accessModifier.bai_tap;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle c = new Circle();
+
+        System.out.println(c.getArea());
+        System.out.println(c.getRadius());
+    }
+}
